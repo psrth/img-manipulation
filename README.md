b621fda748e1846256ce8bd9dabd079ad82f0ee2
Parth Sharma <parthsharma151@gmail.com>
2021-04-08 15:07:53 +0530

    edit: comments, instructions

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Image Manipulation Suite
-**a collection of python scripts to automate image manipulation that I need on a regular basis.**
+**a collection of python scripts to automate image manipulation that I need on a regular basis.**  
+
+**BEFORE USING ANY SCRIPT CHANGE THE PATH TO ABSOLUTE PATH OF CLONED REPO.**
 
 ## Image-to-PNG
 1. Put any image/group of images in the `img2png` directory.  
 2. Run `img2png.py`  
 3. Find all converted PNGs in the pngs folder.
 
-## 
+## Logos
+Add files to logos directory. Run the script. Find the converted logos in ./logo.
--- logos/logos.py
+++ logos/logos.py
@@ -1,24 +1,34 @@
 from PIL import Image, ImageOps
 import os
 
+# max size of logo
 size_300 = (300, 300)
 
+# max size of final image
+new_size = (400, 400)
+
 for f in os.listdir('/Users/Parth/development/github/img-manipulation/logos'):
+    # load files that are image types
     if (f.endswith('.jpg') or f.endswith('.png') or f.endswith('.webp') or f.endswith('.JPG') or f.endswith('.PNG') or f.endswith('.WEBP')):
         i = Image.open(f)
+        fn, fext = os.path.splitext(f)
+
+        # convert PNGs to JPGs with a white background
         new_image = Image.new("RGBA", i.size, "WHITE") 
         new_image.paste(i, (0, 0), i.convert("RGBA")) 
         i = new_image
         i = i.convert("RGB")
-        fn, fext = os.path.splitext(f)
+        
+        # resize images to max value of 300x300
         i.thumbnail(size_300)
         old_size = i.size
 
-        new_size = (400, 400)
+        # apply uniform border on new image
         new_im = Image.new("RGB", new_size, "white")
         new_im.paste(i, ((new_size[0]-old_size[0])//2,
                       (new_size[1]-old_size[1])//2))
 
+        # save image
         new_im.save('/Users/Parth/development/github/img-manipulation/logos/logo/{}.png'.format(fn))
         
         
